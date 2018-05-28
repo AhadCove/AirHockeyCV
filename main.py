aecc78e076465ed5162897c26edde37196dd0485
AhadCove <ahadcove@gmail.com>
2018-05-28 14:48:06 -0400

    First push

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import glob
+import cv2
+
+img = cv2.imread('./Images/BasicBoard.png')
+
+# We want to convert the colors to HSV to work with
+hsvImg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+# Since we know the value won't change at all we can keep the masks the same
+lower = np.array([0, 0, 255])
+higher = np.array([0, 0, 255])
