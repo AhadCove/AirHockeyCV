1b4657e2d07fd94f65ecd71a01160e07c1f5d454
AhadCove <ahadcove@gmail.com>
2018-05-28 15:07:00 -0400

    First push

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ hsvImg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 # Since we know the value won't change at all we can keep the masks the same
 lower = np.array([0, 0, 255])
 higher = np.array([0, 0, 255])
+
+puckMask = cv2.inRange(hsvImg, lower, higher)
+
+cv2.imshow("Original", img)
+cv2.imshow("Puck Mask", puckMask)
+cv2.waitKey(0)
