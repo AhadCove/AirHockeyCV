1eb9ab0da72797e09590e800d33cd8574b30037d
AhadCove <ahadcove@gmail.com>
2018-05-31 22:15:28 -0400

    3 using Morphology to massage mask

--- main.py
+++ main.py
@@ -4,62 +4,57 @@ import cv2
 
 img = cv2.imread('./Images/BasicBoard.png')
 
-
-# We want to convert the colors to HSV to work with
 hsvImg = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-# define range of white color in HSV of puck
 lower = np.array([0, 0, 255])
 higher = np.array([0, 0, 255])
 
-# Threshold the HSV image to get only puck color
 puckMask = cv2.inRange(hsvImg, lower, higher)
 
-# Reverse the image colors
-# puckMask = cv2.bitwise_not(puckMask)
 
-# Setup BlobDetector
-params = cv2.SimpleBlobDetector_Params() # 2.x version should use cv2.SimpleBlobDetector()
+# Get rid of noise
+kernel = np.ones((5,5), np.uint8)
+puckMask = cv2.dilate(puckMask, None, iterations=2) 
+puckMask = cv2.erode(puckMask, kernel, iterations=1)
 
-# Threshold: min pixel intensity of interest
-# params.minThreshold = 150
-# params.maxThreshold = 220  
+puckMask = cv2.morphologyEx(puckMask, cv2.MORPH_OPEN, kernel)
+puckMask = cv2.morphologyEx(puckMask, cv2.MORPH_CLOSE, kernel)
 
-# Get white colors
-params.filterByColor = True
-params.blobColor = 255
+cnts = cv2.findContours(puckMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[1] # You should get first index if using Opencv2
 
-# Filter by Area.
-params.filterByArea = True
-params.minArea = 20
-params.maxArea = 1000
-	 
-# Filter by Circularity
-params.filterByCircularity = True
-params.minCircularity = 0.10
+print("Counts", cnts)
+
+# loop over the contours
+for c in cnts:
+	# compute the center of the contour
+	M = cv2.moments(c)
+	cX = int(M["m10"] / M["m00"])
+	cY = int(M["m01"] / M["m00"])
+
+	# draw the contour and center of the shape on the image
+	cv2.drawContours(img, [c], -1, (0, 255, 0), 2)
+	
+  # cv2.circle(img, (cX, cY), 7, (0, 0, 255), -1)
+	# cv2.putText(img, "center", (cX - 20, cY - 20),
+  # cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+  
+# params = cv2.SimpleBlobDetector_Params()
+# params.filterByColor = True
+# params.blobColor = 255
+
+# params.filterByArea = True
+# params.minArea = 1
+# params.maxArea = 100000
+
+# params.filterByCircularity = False
+# params.filterByConvexity = False
+# params.filterByInertia = False
  
-# Filter by Convexity
-params.filterByConvexity = False
-# params.minConvexity = 0.87
-	 
-# Filter by Inertia
-params.filterByInertia = False
-# params.minInertiaRatio = 0.8
+# detector = cv2.SimpleBlobDetector_create(params)
+# keypoints = detector.detect(puckMask)
+# blobPoints = cv2.drawKeypoints(puckMask, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
-# Distance Between Blobs
-# params.minDistBetweenBlobs = 0
- 
-# Create a detector with the parameters
-detector = cv2.SimpleBlobDetector_create(params)
-
-keypoints = detector.detect(puckMask)
-
-# Draw detected blobs as red circles.
-# Ensures the size of the circle corresponds to the size of blob
-blobPoints = cv2.drawKeypoints(puckMask, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-cv2.imshow("Original", img)
-cv2.imshow("Puck Mask", puckMask)
-cv2.imshow("Blob Points", blobPoints)
+# cv2.imshow("Blob Points", blobPoints)
+cv2.imshow("Image", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
